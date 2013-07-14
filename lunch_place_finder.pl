8a9e7ab72df0124ca573c3d579be3251e3f16fe7
Arne Schroppe <arne.schroppe@gmx.de>
2013-07-14 19:35:44 +0200

    Cleanup

--- lunch_place_finder.pl
+++ lunch_place_finder.pl
@@ -7,18 +7,15 @@ have_lunch_at(People, Places) :-
   nonvar(People),
   setof(Y, foreach(member(X, People), would_eat_at(X, Y)), Places),
   !.
-
 have_lunch_at(People, Places) :-
   nonvar(Places),
   setof(X, foreach(member(Y, Places), (person(X), would_eat_at(X, Y))), People),
   !.
 
-
 have_lunch_at(People, Places, LongLunchOk) :-
   nonvar(People),
   setof(Y, foreach(member(X, People), (would_eat_at(X, Y), location_ok(Y, LongLunchOk))), Places),
   !.
-
 
 location_ok(Place, LongLunchOk) :-
     not(LongLunchOk),
@@ -26,18 +23,16 @@ location_ok(Place, LongLunchOk) :-
 location_ok(_Place, LongLunchOk) :-
     LongLunchOk.
 
-
 would_eat_at(Person, Place) :-
   restaurant(Place),
   fits_vegetarian_preferences(Person, Place),
   fits_general_preferences(Person, Place).
 
+fits_vegetarian_preferences(Person, _) :-
+  not(vegetarian(Person)).
 fits_vegetarian_preferences(Person, Place) :-
   vegetarian(Person),
   not(bad_for_vegetarians(Place)).
-
-fits_vegetarian_preferences(Person, _) :-
-  not(vegetarian(Person)).
 
 fits_general_preferences(Person, Place) :-
   not(dislikes(Person, Place)).
